f28c0ce461fa96be94f67bdb84bb1eaa3c5185ec
ronngrong <2421155310@qq.com>
2023-08-21 19:12:47 +0800

    radio-group 组件增加备注

--- src/components/radio/radio-group.vue
+++ src/components/radio/radio-group.vue
@@ -38,6 +38,7 @@ export default {
   },
   
   computed: {
+    // 计算包裹 radio-group 组件的标签
     _elTag() {
       let tag = (this.$vnode.data || {}).tag;
       if (!tag || tag === 'component') tag = 'div';
@@ -61,7 +62,7 @@ export default {
   },
   
   mounted() {
-    console.log('mounted')
+    // 如果 radio-group 按钮组没有默认的选项时，将第一个 radio 的 tabIndex 设置为0。代表元素是可聚焦的
     const radios = this.$el.querySelectorAll('[type=radio]');
     const firstLabel = this.$el.querySelectorAll('[role=radio]')[0];
     if (![].some.call(radios, radio => radio.checked) && firstLabel) {
@@ -71,17 +72,24 @@ export default {
   
   methods: {
     handleKeydown(e) {
+      // 用 target 变量保存事件触发的元素
       const target = e.target;
+      // 判断当前触发的是 input 元素还是 label 元素
       const className = target.nodeName === 'INPUT' ? '[type=radio]' : '[role=radio]';
+      // 获取到当前组件模板根标签下的所有同类型元素
+      // this.$el 指向当前组件模板的根标签
       const radios = this.$el.querySelectorAll(className);
       const length = radios.length;
+      // 获取触发的元素的索引值
       const index = [].indexOf.call(radios, target);
+      // 获取到当前组件模板根标签下的全部 role=radio 的元素
       const roleRadios = this.$el.querySelectorAll('[role=radio]');
       switch (e.keyCode) {
         case keyCode.LEFT:
         case keyCode.UP:
           e.stopPropagation();
           e.preventDefault();
+          // 按下左或上，则让前一个元素点击并聚焦，如果当前是第一个元素，则让最后一个元素点击并聚焦
           if (index === 0) {
             roleRadios[length - 1].click();
             roleRadios[length - 1].focus();
@@ -92,6 +100,7 @@ export default {
           break;
         case keyCode.RIGHT:
         case keyCode.DOWN:
+          // 按下右或下，则让下一个元素点击并聚焦，如果当前是最后一个元素，则让第一个元素点击并聚焦
           if (index === (length - 1)) {
             e.stopPropagation();
             e.preventDefault();
